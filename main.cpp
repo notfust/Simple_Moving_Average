0ab4d60ecdd2fa171685da0e4a357ea0daae30ad
Roman <95196104+notfust@users.noreply.github.com>
2023-08-10 11:20:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,35 +3,12 @@
 #include <vector>
 #include <chrono>
 #include <fstream>
-#include <queue>
+#include "window.h"
 
-#define ARR_SIZE 1'000'000
+static constexpr std::size_t ARR_SIZE = 1'000'000;
 
 using namespace std::chrono;
 using std::vector;
-
-template<class T>
-class Window {
-public:
-    Window(std::size_t size) : size (size), sumX (0) {
-        for (std::size_t i = 0; i < size; i++) queue.push(0);
-    }
-
-    T next(T value) {
-        sumX -= queue.front();
-        queue.pop();
-
-        sumX += value;
-        queue.push(value);
-
-        return sumX / size;
-    }
-
-private:
-    std::size_t size;
-    std::queue<T> queue;
-    T sumX;
-};
 
 int main() {
     std::random_device rd;
@@ -42,17 +19,19 @@ int main() {
     std::ofstream tableTimes("timetests.csv", std::ofstream::out | std::ofstream::trunc);
     std::ofstream tableValues("value.csv", std::ofstream::out | std::ofstream::trunc);
 
-    tableTimes << "Type w/ window" << ',' << "count/sec" << std:: endl;
-    tableValues << "Type w/ window" << ',';
-    for (std::size_t i = 0; i < 1000; i++) tableValues << i + 1 << ',';
+    tableTimes << "type_and_window" << ',' << "count_sec" << std:: endl;
+    tableValues << "type_and_window" << ',';
+    for (std::size_t i = 0; i < 1000; i++) 
+        tableValues << i + 1 << ',';
     tableValues << std::endl;
 
     vector<float> floatInputVector(ARR_SIZE);
 
-    tableValues << "Float (init)" << ',';
+    tableValues << "float_init" << ',';
     for (std::size_t i = 0; i < ARR_SIZE; i++) {
         floatInputVector[i] = dist(rd);
-        if (i < 1000) tableValues << floatInputVector[i] << ',';
+        if (i < 1000) 
+            tableValues << floatInputVector[i] << ',';
     }
     tableValues << std::endl;
 
@@ -61,24 +40,27 @@ int main() {
         vector<float> floatOutputVector(ARR_SIZE);
 
         start = steady_clock::now();
-        for (std::size_t j = 0; j < ARR_SIZE; j++) floatOutputVector[j] = window.next(floatInputVector[j]);
+        for (std::size_t j = 0; j < ARR_SIZE; j++) 
+            floatOutputVector[j] = window.next(floatInputVector[j]);
         end = steady_clock::now();
 
-        tableTimes << "Float (" << i << ")" << ','
+        tableTimes << "float_" << i << ','
                    << ARR_SIZE / duration<double>(end - start).count()
                    << std::endl;
 
-        tableValues << "Float (" << i << ")" << ',';
-        for (std::size_t j = 0; j < 1000; j++) tableValues << floatOutputVector[j] << ',';
+        tableValues << "float_" << i << ',';
+        for (std::size_t j = 0; j < 1000; j++) 
+            tableValues << floatOutputVector[j] << ',';
         tableValues << std::endl;
     }
 
     vector<double> doubleInputVector(ARR_SIZE);
 
-    tableValues << "Double (init)" << ',';
+    tableValues << "double_init" << ',';
     for (std::size_t i = 0; i < ARR_SIZE; i++) {
         doubleInputVector[i] = dist(rd);
-        if (i < 1000) tableValues << doubleInputVector[i] << ',';
+        if (i < 1000) 
+            tableValues << doubleInputVector[i] << ',';
     }
 
     tableValues << std::endl;
@@ -87,15 +69,17 @@ int main() {
         vector<double> doubleOutputVector(ARR_SIZE);
 
         start = steady_clock::now();
-        for (std::size_t j = 0; j < ARR_SIZE; j++) doubleOutputVector[j] = window.next(floatInputVector[j]);
+        for (std::size_t j = 0; j < ARR_SIZE; j++) 
+            doubleOutputVector[j] = window.next(floatInputVector[j]);
         end = steady_clock::now();
 
-        tableTimes << "Double (" << i << ")" << ','
+        tableTimes << "double_" << i << ','
                    << ARR_SIZE / duration<double>(end - start).count()
                    << std::endl;
 
-        tableValues << "Double (" << i << ")" << ',';
-        for (std::size_t j = 0; j < 1000; j++) tableValues << doubleOutputVector[j] << ',';
+        tableValues << "double_" << i << ',';
+        for (std::size_t j = 0; j < 1000; j++) 
+            tableValues << doubleOutputVector[j] << ',';
         tableValues << std::endl;
     }
 
